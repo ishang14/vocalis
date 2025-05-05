1a36afa05448d8bb74d9e6f314764ed0fb8cf1a1
ishang14 <igarg142001@gmail.com>
2025-05-05 17:12:55 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,12 +34,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../../frontend/dist/index.html"));
   });
 }
+
 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
